cd0bf57bba5ade199bd36a85c42b1c2ba197a332
Tomas36M <tomasmunevar36@gmail.com>
2025-05-23 18:06:56 -0500

    first commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,13 @@
 import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom'; // Import correcto de React Router
 import styles from './Navbar.module.css';
 
 export default function Navigation() {
     return (
         <Navbar expand="lg" fixed="top" className={styles.navbar}>
             <Container fluid>
-                <Navbar.Brand href="/" className={styles.brand}>
+                {/* Brand como Link de React Router */}
+                <Navbar.Brand as={Link} to="/" className={styles.brand}>
                     <span className={styles.name}>Isabela Munevar</span>
                     <span className={styles.title}>Ph.D. Economics</span>
                 </Navbar.Brand>
@@ -14,10 +16,34 @@ export default function Navigation() {
                 
                 <Navbar.Collapse id="navbar" className={styles.navCollapse}>
                     <Nav className={`ms-auto ${styles.navLinks}`}>
-                        <Nav.Link href="research" className={styles.navLink}>Research</Nav.Link>
-                        <Nav.Link href="#contact" className={styles.navLink}>Contact</Nav.Link>
-                        <Nav.Link href="https://drive.google.com/file/d/1-3cDnAZkriPOyRf-UxvzcHNYCrDJY17p/view?usp=sharing" target="_blank" className={styles.navLink}>JMP</Nav.Link>
-                        <Nav.Link href="https://drive.google.com/file/d/1-7FQISgfFtMCkf7RBzzbssxEuIuWUS8B/view?usp=sharing" target="_blank" className={styles.navLink}>CV</Nav.Link>
+                        {/* Enlaces internos con Link */}
+                        <Nav.Link as={Link} to="/research" className={styles.navLink}>
+                            Research
+                        </Nav.Link>
+                        
+                        {/* Enlace de contacto interno */}
+                        <Nav.Link as={Link} to="/contact" className={styles.navLink}>
+                            Contact
+                        </Nav.Link>
+                        
+                        {/* Enlaces externos con target y rel */}
+                        <Nav.Link 
+                            href="https://drive.google.com/file/d/1-3cDnAZkriPOyRf-UxvzcHNYCrDJY17p/view?usp=sharing" 
+                            target="_blank" 
+                            rel="noopener noreferrer" 
+                            className={styles.navLink}
+                        >
+                            JMP
+                        </Nav.Link>
+                        
+                        <Nav.Link 
+                            href="https://drive.google.com/file/d/1-7FQISgfFtMCkf7RBzzbssxEuIuWUS8B/view?usp=sharing" 
+                            target="_blank" 
+                            rel="noopener noreferrer" 
+                            className={styles.navLink}
+                        >
+                            CV
+                        </Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
